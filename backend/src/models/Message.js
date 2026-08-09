21770f48062b9c64e9fba2d1ec446e5774f7d356
omjadhav-dev <omsjadhav1991@gmail.com>
2026-08-09 11:08:48 +0530

    Message model

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,35 +1,18 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
 
-const messageSchema = new mongoose.Schema(
+const Message = sequelize.define(
+  'Message',
   {
-    // A conversation is uniquely identified by two user IDs
-    // We store both to query easily
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    // conversationId is just a sorted combo of both user IDs
-    // so we can fetch all messages between two users quickly
-    conversationId: {
-      type: String,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    read: {
-      type: Boolean,
-      default: false,
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    senderId: { type: DataTypes.INTEGER, allowNull: false },
+    receiverId: { type: DataTypes.INTEGER, allowNull: false },
+    // sorted combo of both user IDs, so A-B and B-A give the same conversation
+    conversationId: { type: DataTypes.STRING, allowNull: false },
+    text: { type: DataTypes.TEXT, allowNull: false },
+    read: { type: DataTypes.BOOLEAN, defaultValue: false },
   },
-  { timestamps: true }
+  { tableName: 'messages' }
 );
 
-module.exports = mongoose.model('Message', messageSchema);
+module.exports = Message;
