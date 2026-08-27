17c2b314477a43ac53a7331d4859531692c1a0e0
omjadhav-dev <omsjadhav1991@gmail.com>
2026-08-27 17:38:30 +0530

    Products model update

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -9,6 +9,7 @@ const Product = sequelize.define(
     description: { type: DataTypes.TEXT, defaultValue: '' },
     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false, validate: { min: 0 } },
     stock: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0, validate: { min: 0 } },
+    lowStockThreshold: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 5, validate: { min: 0 } },
     unit: { type: DataTypes.STRING, defaultValue: 'piece' },
     category: { type: DataTypes.STRING, defaultValue: 'General' },
     image: { type: DataTypes.STRING, defaultValue: '' },
